d07e2ce601ea86983193f0828fc1add3a2f00c69
Lakos Bálint <csalakos1@gmail.com>
2025-11-09 12:30:31 +0100

    Order, Cart, Search Engine

---
+++ Libratica.DataContext/DTOs/AddToCartDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Libratica.DataContext.DTOs
+{
+    public class AddToCartDto
+    {
+        [Required(ErrorMessage = "Hirdetés ID kötelező")]
+        public int ListingId { get; set; }
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "Mennyiség 1-100 között lehet")]
+        public int Quantity { get; set; } = 1;
+    }
+}
---
+++ Libratica.DataContext/DTOs/OrderDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libratica.DataContext.DTOs
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public UserDto Buyer { get; set; } = null!;
+        public UserDto Seller { get; set; } = null!;
+        public List<OrderItemDto> Items { get; set; } = new();
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; } = string.Empty; // pending, paid, shipped, delivered, cancelled
+        public string? ShippingAddress { get; set; }
+        public string? PaymentMethod { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class OrderItemDto
+    {
+        public int Id { get; set; }
+        public BookDto Book { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal PriceAtPurchase { get; set; }
+        public decimal Subtotal => PriceAtPurchase * Quantity;
+    }
+}

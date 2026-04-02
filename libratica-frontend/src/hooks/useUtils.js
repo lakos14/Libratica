a426626527e4c5703efc6d6595c482283568a25a
valeo <valeo@DESKTOP-4ODI9PR>
2026-04-02 11:24:45 +0200

    bugfix

--- libratica-frontend/src/hooks/useUtils.js
+++ libratica-frontend/src/hooks/useUtils.js
@@ -1,16 +1,15 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { queryKeys } from '../lib/queryClient';
-import { usersAPI, recommendationsAPI, bookCollectionAPI, aiAPI, reportsAPI } from '../services/api';
+import api, { usersAPI, recommendationsAPI, bookCollectionAPI, aiAPI, reportsAPI, BASE_URL } from '../services/api';
 import { toast } from 'react-toastify';
-import { BASE_URL } from '../services/api';
 
 
 export const useCategories = () => {
   return useQuery({
     queryKey: queryKeys.categories,
     queryFn: async () => {
-      const response = await fetch(`${BASE_URL}/api/categories`);
-      return response.json();
+      const response = await api.get('/categories');
+      return response.data;
     },
     staleTime: 30 * 60 * 1000,
   });

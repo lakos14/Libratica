6c94978adc5c80dcd9290458769e2468e726a003
valeo <valeo@DESKTOP-4ODI9PR>
2026-04-02 12:10:20 +0200

    checkout dto bugfix

--- Libratica.DataContext/DTOs/CheckoutDto.cs
+++ Libratica.DataContext/DTOs/CheckoutDto.cs
@@ -4,14 +4,17 @@ namespace Libratica.DataContext.DTOs
 {
     public class CheckoutDto
     {
+        [Range(1, int.MaxValue, ErrorMessage = "Érvénytelen eladó")]
         public int SellerId { get; set; }
 
         [Required]
+        [MinLength(1, ErrorMessage = "Legalább egy tétel szükséges")]
         public List<CheckoutItemDto> Items { get; set; } = new();
 
         [Required]
         public string ShippingAddress { get; set; } = string.Empty;
 
+        [RegularExpression("^(cash|transfer)$", ErrorMessage = "Érvénytelen fizetési mód")]
         public string? PaymentMethod { get; set; }
     }
 
